1f7753224bdb9b045372a699aa256bb82f95c8ab
ArifRahman <azrif27018@gmail.com>
2026-04-27 11:06:18 +0700

    Perbaikian upload dokumen

--- lib/services/api_config.dart
+++ lib/services/api_config.dart
@@ -1,13 +1,21 @@
 class ApiConfig {
   static const String baseUrl =
+<<<<<<< HEAD
       "https://27ff-103-166-161-78.ngrok-free.app/api";
+=======
+      "https://trade-lusty-doorbell.ngrok-free.dev/api";
+>>>>>>> b2ea2d1 (Perbaikian upload dokumen)
 
   static const login = "/login";
   static const register = "/register";
 
   static const submitPengajuan = "/pengajuan/submit";
   static const checkDomain = "/pengajuan/check-domain";
+<<<<<<< HEAD
   // ADMIN
   static const getPengajuan = "/admin/pengajuan";
   static const aktivasi = "/admin/aktivasi";
 }
+=======
+}
+>>>>>>> b2ea2d1 (Perbaikian upload dokumen)
